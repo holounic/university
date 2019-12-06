da80522b87a13ecd82779713533c0399b0020d1e
Masha Samsikova <41171543+holounic@users.noreply.github.com>
2019-12-06 18:48:55 +0300

    Delete Board.java

--- paradigms/semester-1/some-code/Board.java
+++
@@ -1,7 +0,0 @@
-package mnkGame;
-
-public abstract class Board {
-    protected abstract Position getPosition();
-    protected abstract Cell getCell();
-    protected abstract Result makeMove(Move move);
-}
