6cfcd66f8318e3bdd4114c64111b8b76f09781e0
Masha Samsikova <41171543+holounic@users.noreply.github.com>
2020-02-06 10:30:53 +0300

    CheckedNegate class fixed

--- paradigms/semester-1/some-code/expression/CheckedNegate.java
+++ paradigms/semester-1/some-code/expression/CheckedNegate.java
@@ -2,10 +2,14 @@ package expression;
 
 public class CheckedNegate implements Operand {
     private static final Priority priority = Priority.VAR;
-    private final String variable;
+    private final Variable variable;
     private static final char operationSign = '-';
 
     public CheckedNegate(String variable) {
+        this.variable = new Variable(variable);
+    }
+
+    public CheckedNegate(Variable variable) {
         this.variable = variable;
     }
 
@@ -17,7 +21,7 @@ public class CheckedNegate implements Operand {
 
     @Override
     public int evaluate(int x) throws ArithmeticException {
-        if (this.variable.equals("x")) {
+        if (this.variable.evaluate(10) == 10) {
             overflowCheck(x);
             return -x;
         }
@@ -26,14 +30,14 @@ public class CheckedNegate implements Operand {
 
     @Override
     public int evaluate(int x, int y, int z) {
-        switch (this.variable) {
-            case ("x"):
+        switch (this.variable.evaluate(1, 2, 3)) {
+            case (1):
                 overflowCheck(x);
                 return -x;
-            case ("y"):
+            case (2):
                 overflowCheck(y);
                 return -y;
-            case("z"):
+            case(3):
                 overflowCheck(z);
                 return -z;
                 default:
