56b2ebd870257e58b9a090909c5f1ae71b77d19b
Masha Samsikova <41171543+holounic@users.noreply.github.com>
2020-10-28 12:16:16 +0300

    Update iv.sh

--- OS/lab3/iv.sh
+++ OS/lab3/iv.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-script="./iv_proc.sh"
+script="iv_proc.sh"
 chmod +x $script
 
 {
-    $script &
-    $script &
-    $script & 
+    "./$script" &
+    "./$script" &
+    "./$script" & 
 } ;
-proc_pid=`ps -ao pid -C $script --no-header | head -1` ;
+proc_pid=`pgrep iv_proc.sh -o` ;
 sudo cpulimit --pid $proc_pid --limit 5 --background
